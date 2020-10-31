4a44c98b10f52fea91c7234619c09881120e8e32
Norberto Cabrera Alba <cabrera.norberto@hotmail.com>
2020-10-31 01:43:31 -0600

    Add files via upload

---
+++ Exercise 3-1 - Development C/Exercise 3-1 - Development C/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace Exercise_3_1___Development_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var price="0";
+
+            string Reader(string name)
+            {
+                string fileName = "breakfast.xml";
+                string path = Path.Combine(Environment.CurrentDirectory, fileName);
+                
+                XmlTextReader xmlReader = new XmlTextReader(path);
+                List<string> names = new List<string>();
+                List<string> values = new List<string>();
+
+                while (xmlReader.Read())
+                {
+                    switch (xmlReader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            names.Add(xmlReader.Name);
+                            break;
+                        case XmlNodeType.Text:
+                            values.Add(xmlReader.Value);
+                            break;
+                    }
+                }
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    
+                    if (values[i] == name)
+                    {
+                        Console.WriteLine("Name of the Product: " + values[i]);
+                        price = values[i+1];
+                        
+                    }
+                }
+                return price;
+            }
+
+            Console.WriteLine("Price: " + Reader("French Toast"));
+        }
+    }
+}
